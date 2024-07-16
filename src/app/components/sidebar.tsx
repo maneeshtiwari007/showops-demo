4f47976fd26723e7e976c290e381d65b9f9d6c39
maneeshtiwari007 <maneeshtiwari007@gmail.com>
2024-07-16 18:08:27 +0530

    commit for data
    commit for data

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -9,14 +9,27 @@ import { useTheme } from 'next-themes';
 import Link from 'next/link';
 import style from './sidebar.module.css'
 import LinkWithoutBox from './common/LinkWithoutBox';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
 import NextLink from "next/link";
+import { endpoint } from '@/utils/data';
 const Sidebar = ({ data }: LayoutPropsInterface) => {
     const { theme, systemTheme, setTheme } = useTheme();
     let [themeState, setThemeState] = useState((theme !== 'dark') ? false : true);
+    const [eventData,setEventData]:any = useState(undefined);
     const router = usePathname();
-    console.log(themeState)
+    async function getAllTodaysEvent() {
+        const data = await fetch(`${endpoint}/api/events`);
+        if (!data.ok) {
+          throw new Error('Failed to fetch data')
+        }
+        data.json().then((response)=>{
+            setEventData(response?.data);
+        })
+    }
+    useEffect(()=>{
+        getAllTodaysEvent();
+    },[])
     return (
         <>
             <div className='SidebarArea'>
@@ -46,63 +59,27 @@ const Sidebar = ({ data }: LayoutPropsInterface) => {
                             </div>
                             <Section style={{ padding: '1rem' }}>
                                 <Heading as="h6" color="gray" style={{ marginBottom: '1rem' }}>Today&apos;s event</Heading>
-                                <Box style={{ '--card-border-width': '0' } as any} mb="3">
+                                {eventData?.map((item:any,index:number) => {
+                                    return <Box style={{ '--card-border-width': '0' } as any} mb="3" key={index}>
                                     <Card style={{ '--card-border-width': '0' } as any} variant='ghost'>
                                         <Flex gap="3" align="center">
                                             <Avatar
                                                 size="3"
-                                                src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+                                                src={item?.image}
                                                 fallback="A"
                                             />
                                             <Box>
                                                 <Text as="div" size="2" color="gray">
-                                                    Tourist
+                                                {item?.name}
                                                 </Text>
                                                 <Text as="div" size="2" weight="bold">
-                                                    The Viper Room
+                                                {item?.description}
                                                 </Text>
                                             </Box>
                                         </Flex>
                                     </Card>
                                 </Box>
-                                <Box style={{ '--card-border-width': '0' } as any} mb="3">
-                                    <Card style={{ '--card-border-width': '0' } as any} variant='ghost'>
-                                        <Flex gap="3" align="center">
-                                            <Avatar
-                                                size="3"
-                                                src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
-                                                fallback="A"
-                                            />
-                                            <Box>
-                                                <Text as="div" size="2" color="gray">
-                                                    Tourist
-                                                </Text>
-                                                <Text as="div" size="2" weight="bold">
-                                                    The Viper Room
-                                                </Text>
-                                            </Box>
-                                        </Flex>
-                                    </Card>
-                                </Box>
-                                <Box style={{ '--card-border-width': '0' } as any} mb="3">
-                                    <Card style={{ '--card-border-width': '0' } as any} variant='ghost'>
-                                        <Flex gap="3" align="center">
-                                            <Avatar
-                                                size="3"
-                                                src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
-                                                fallback="A"
-                                            />
-                                            <Box>
-                                                <Text as="div" size="2" color="gray">
-                                                    Tourist
-                                                </Text>
-                                                <Text as="div" size="2" weight="bold">
-                                                    The Viper Room
-                                                </Text>
-                                            </Box>
-                                        </Flex>
-                                    </Card>
-                                </Box>
+                                })}
                             </Section>
                         </div>
                         <Section pt={"1"} pb={"1"} pl={"5"} position={"absolute"} bottom="5" left="0" width={"100%"}>
