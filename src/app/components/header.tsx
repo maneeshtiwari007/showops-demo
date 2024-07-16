5ee5a2117b2c6e432d7265560aefd8ef0c5de4f5
subhash018 <59439792+subhash018@users.noreply.github.com>
2024-07-16 18:32:06 +0530

    Added search form
     search form design added

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -21,60 +21,61 @@ export default function Header({ data, ghost }: HeaderProps) {
     <>
       <div className={classNames(styles.HeaderRoot, ghost ? styles.ghost : "")}>
         <div className={styles.HeaderInner}>
-          <Grid columns={{ initial: '2', md: '1' }} style={{ justifyContent: 'space-between' }} gap="3" width="auto">
-
-            <Flex
-              display={{ initial: "flex", md: "none" }}
-              align="center">
-              <div>
-                <input type="search" className="Input" required placeholder="Search ShowOps..." />
-              </div>
-            </Flex>
-
-            <Flex
-              display={{ md: "none" }}
-              pt={"4"}
-              pb={"4"}
-            >
-              <div>
-                <Button className="ghost_left" variant="soft" color="gray"><ChevronLeftIcon width={18} height={18} /></Button>
-              </div>
-            </Flex>
+          <Grid columns={{ initial: '2', md: '2' }} style={{ justifyContent: 'space-between' }} gap="3" width="auto">
 
             <Flex
               display={{ initial: "none", md: "flex" }}
-              align="center"
-              gap="5"
-              pr="4"
-              justify={"end"}
-              pt={"4"}
-              pb={"4"}
-            >
-              <div>
-                <Button variant="soft" color="gray"><BellIcon width={18} height={18} /></Button>
-              </div>
+              align="center">
+              <div className="FormControlWrapper searchForm">
+                <MagnifyingGlassIcon />
+                <input type="search" className="Input" required placeholder="Search ShowOps..." />
+              </div>              
+          </Flex>
 
-              <Avatar
-                size="2"
-                src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
-                fallback="A"
-              />
-            </Flex>
+          <Flex
+            display={{ md: "none" }}
+            pt={"4"}
+            pb={"4"}
+          >
+            <div>
+              <Button className="ghost_left" variant="soft" color="gray"><ChevronLeftIcon width={18} height={18} /></Button>
+            </div>
+          </Flex>
 
-            <Flex
-              display={{ md: "none" }}
-              align="center"
-              justify="end"
-              gap="4"
-            >
-              <Button className="nav_bar_drawer" variant="soft" style={{ paddingLeft: 10, paddingRight: 10, paddingTop: 8, paddingBottom: 8 }}>
-                <HamburgerMenuIcon width="20" height="20" />
-              </Button>
-            </Flex>
+          <Flex
+            display={{ initial: "none", md: "flex" }}
+            align="center"
+            gap="5"
+            pr="4"
+            justify={"end"}
+            pt={"4"}
+            pb={"4"}
+          >
+            <div>
+              <Button variant="soft" color="gray"><BellIcon width={18} height={18} /></Button>
+            </div>
 
-          </Grid>
-        </div>
-      </div >
+            <Avatar
+              size="2"
+              src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+              fallback="A"
+            />
+          </Flex>
+
+          <Flex
+            display={{ md: "none" }}
+            align="center"
+            justify="end"
+            gap="4"
+          >
+            <Button className="nav_bar_drawer" variant="soft" style={{ paddingLeft: 10, paddingRight: 10, paddingTop: 8, paddingBottom: 8 }}>
+              <HamburgerMenuIcon width="20" height="20" />
+            </Button>
+          </Flex>
+
+        </Grid>
+      </div>
+    </div >
     </>
   );
 }
